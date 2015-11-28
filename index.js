1e5115de042e41fc69ffae8b10c6a5cb08971e2b
Eric Craft <eacraft13@gmail.com>
2015-11-28 15:50:01 -0800

    Base setup

---
+++ index.js
@@ -0,0 +1,27 @@
+global.__base = __dirname + '/';
+
+var cluster = require('cluster');
+var os      = require('os');
+
+
+
+// Master
+if (cluster.isMaster) {
+    for (var i = 0; i < os.cpus().length; i++)
+        cluster.fork();
+
+    // Replace a dead worker
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker', worker.process.pid, 'died with code', code, '-- restarting...');
+        cluster.fork();
+    });
+}
+
+
+
+// Children
+else {
+    var config = require('/etc/configs/app');
+    var app    = require('./lib/index')(config);
+    app.listen(config.port);
+}
