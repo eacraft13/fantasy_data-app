e917590f698cc1951feed22d8ea641bb44e65d23
Eric Craft <eacraft13@gmail.com>
2015-12-03 06:52:50 -0800

    remove unneededd directories

--- index.js
+++ index.js
@@ -1,25 +1,76 @@
-var cluster = require('cluster');
-var os      = require('os');
+var assets         = require('connect-assets');
+var express        = require('express');
+var favicon        = require('serve-favicon');
+var helmet         = require('helmet');
+var httpProxy      = require('http-proxy');
+var HttpProxyRules = require('http-proxy-rules');
 
 
 
-// Master
-if (cluster.isMaster) {
-    for (var i = 0; i < os.cpus().length; i++)
-        cluster.fork();
+module.exports = function(config) {
+    var app   = express();
+    var proxy = httpProxy.createProxyServer();
 
-    // Replace a dead worker
-    cluster.on('exit', function (worker, code, signal) {
-        console.log('Worker', worker.process.pid, 'died with code', code, '-- restarting...');
-        cluster.fork();
-    });
-}
+    app.use(favicon('./lib/views/_images/favicon.ico'));
+    app.use('/images', express.static('./lib/views/_images'));
+    app.use(helmet());
+
+    app.set('views', './lib/views');
+    app.set('view engine', 'jade');
+
+    app.use(assets({
+        paths: ['./lib/views']
+    }));
 
 
 
-// Children
-else {
-    var config = { app: require('/etc/configs/app'), proxy: require('/etc/configs/proxy') };
-    var app    = require('./lib/index')(config);
-    app.listen(config.app.port);
-}
+    // App routes
+    app
+
+    // Funnel
+    .get('/', handler('landing'))
+
+    // Dashboard
+    .get('/dashboard',                                handler('dashboard'))
+    .get('/dashboard/game-stats/player',              handler('player_game_stats'))
+    .get('/dashboard/game-stats/player-projected',    handler('player_projected_game_stats'))
+    .get('/dashboard/game-stats/team',                handler('team_game_stats'))
+    .get('/dashboard/game-stats/defense',             handler('defense_game_stats'))
+    .get('/dashboard/game-stats/defense-projected',   handler('defense_projected_game_stats'))
+    .get('/dashboard/season-stats/player',            handler('player_season_stats'))
+    .get('/dashboard/season-stats/player-projected',  handler('player_projected_season_stats'))
+    .get('/dashboard/season-stats/team',              handler('team_season_stats'))
+    .get('/dashboard/season-stats/defense',           handler('defense_season_stats'))
+    .get('/dashboard/season-stats/defense-projected', handler('defense_projected_season_stats'))
+
+    // Register
+    .get('/login',  handler('login'))
+    .get('/logout', handler('logout'))
+
+    // Account
+
+    // Proxy
+    .get('/api/*', proxyHandler(config));
+
+
+
+    return app;
+
+
+
+    function handler(view, options) {
+        return function (req, res) {
+            res.render(view, options || {});
+        };
+    }
+
+    function proxyHandler(config) {
+        var target = 'http://' + config.proxy.host + ':' + config.proxy.port;
+        var proxyRules = new HttpProxyRules({ rules: { '/api/*': target } });
+        return function (req, res) {
+            var target = proxyRules.match(req);
+            proxy.web(req, res, { target: target });
+        };
+    }
+
+};
